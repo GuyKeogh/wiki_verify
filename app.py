5c7b5c8e9f9af6963534810f144d56f5d3acf95b
GuyKeogh <guykeogh@protonmail.ch>
2021-06-26 22:55:21 +0100

    Allow articles to be input through URL

--- app.py
+++ app.py
@@ -148,6 +148,77 @@ def article():
                                text=html_output,
                                page=filtered_name,
                                language=language)
+@app.route('/article/<POST_name>')
+def article_named(POST_name):
+    filtered_name = filter_title.filter_title(POST_name) #Removes whitespace, etc
+
+    #Filter bad inputs:
+    title_length = len(filtered_name)
+    if title_length>=256: #Article names must be less than 256 bytes
+        return render_template("index.html",
+                               error_message="An article cannot have this title (too long).")
+    if title_length==0: #Nothing entered
+        return render_template("index.html",
+                               error_message="No title entered.")
+    if filter_title.if_title_invalid_symbol_use(filtered_name): #Problematic symbol use
+        return render_template("index.html",
+                               error_message="An article cannot have this title (invalid symbol use).")
+    
+    if_noun = if_adjective = False
+    if_quote = if_cardinal_number = if_singular_proper_noun = True
+    language = "en"
+
+    #Finished checks
+    #Submit to backend:
+    output = main.main(article_title=filtered_name,
+                        language=language,
+                        if_ignore_URL_error=True,
+                        if_detect_quote=if_quote,
+                        if_detect_CD=if_cardinal_number,
+                        if_detect_NNP=if_singular_proper_noun,
+                        if_detect_NN=if_noun,
+                        if_detect_JJ=if_adjective
+                        )
+    (html_output,external_URLs_failed,data,text_quotes) = output
+    fail_count = len(external_URLs_failed)
+    
+    if fail_count>0: #Create session to request copy-and-paste of failed URLs
+        settings = (language,if_quote,
+                    if_singular_proper_noun,
+                    if_adjective,
+                    if_noun,
+                    if_cardinal_number
+                    )
+        session_ID = str(urandom(24))
+        session_elem = (filtered_name,
+                        data,
+                        text_quotes,
+                        settings,
+                        datetime.now()
+                        )
+        session[session_ID] = session_elem
+        return render_template("article_errors.html",
+                               text=html_output,
+                               page=filtered_name,
+                               language=language,
+                               session_ID=session_ID,
+                               error_count=fail_count,
+                               URLs_failed=external_URLs_failed
+                               )
+    #Using backend output, render HTML:
+    if(html_output=="500"): #Generic server error
+        return render_template("index.html",
+                               error_message = "The article does not exist (title is case-sensitive), or another error occurred.")
+    elif(html_output=="_ERROR: too many external_URLs_"): 
+        max_URL = str(__metadata__.__WEB_EXTERNAL_URL_LIMIT__)
+        error_message = "There are too many citations in the article (over "+max_URL+"). Note: this limit does not apply with the desktop program."
+        return render_template("index.html",
+                               error_message = error_message)
+    else: #Everything is fine
+        return render_template("article.html",
+                               text=html_output,
+                               page=filtered_name,
+                               language=language)
 
 @app.route('/robots.txt')
 def robots():
