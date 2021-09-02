07eae10878bfaeca74a25ce2bc9456a6414bf649
Guy Keogh <guykeogh@protonmail.ch>
2021-09-02 19:06:03 +0100

    Count newlines so they can be used as reference points

--- source/dataparsing/wikitext_extract.py
+++ source/dataparsing/wikitext_extract.py
@@ -4,14 +4,23 @@ __author__ = "Guy Keogh"
 __license__ = "BSD 2-Clause"
 """
 
-def extract_paragraph_numbers(external_URLs, wikitext):
+def count_newlines_before_position(newline_indexes, start_position):
+    index_count = 0
+    for index in newline_indexes:
+        if index > start_position:
+            return index_count
+        index_count+=1
+
+def extract_citation_info(external_URLs, wikitext):
     import re
+
+    newline_indexes = [m.start() for m in re.finditer('\n', wikitext)] #Get position of every \n in the text, as these are useful reference points
 
     #First, find the start locations of every reference:
     reference_starts = [m.start() for m in re.finditer('<ref', wikitext)] #Both normal cites and labels start with '<ref'
     reference_ends = [m.start() for m in re.finditer('</ref>|/>', wikitext)] #Normal cites end in '</ref>', but labels end in '/>'
 
-    citations = [] #List of tuples, with structure: (start_position, end_position, URL, URL_index, label (optional))
+    citations = [] #List of tuples, with structure: (start_position, end_position, URL, how many newlines before start, label (optional))
     label_dictionary = {};
 
     #Check which external_URL falls between the reference start and ends:
@@ -24,14 +33,12 @@ def extract_paragraph_numbers(external_URLs, wikitext):
         citation_info = wikitext[start_position:end_position]
         external_URL = ""
         citation_group = ""
-        URL_index = 0
 
         double_quote_re = r'"([^"]*)"' #Regex pattern to get text between double quote
 
         print(citation_info)
 
         if_URL_found = False
-        URL_index = 0
         for URL in external_URLs:
             if citation_info.find(URL) != -1: #Check if a known external URL is between <ref> and </ref>
                 print("Found %s in the citation template" % (URL,))
@@ -45,14 +52,14 @@ def extract_paragraph_numbers(external_URLs, wikitext):
                     citation_group = citation_group_quote[1:-1] #Remove the double quotes (first and last characters)
                     label_dictionary.update({citation_group: URL})
                 break #Exit the for loop, since we have what we need from the citation
-            URL_index+=1
 
         if not if_URL_found: #No known external URL found in the template; search for a group instead
             label_name_index = citation_info.find('name=')
             if label_name_index != -1:
                 citation_group = citation_group_quote[1:-1] #We now have the group name, so we can look it up in the dictionary once that's fully made
         
-        citations.append(tuple((start_position, end_position, external_URL, URL_index, citation_group)))
+        newline_count = count_newlines_before_position(newline_indexes, start_position)
+        citations.append(tuple((start_position, end_position, external_URL, newline_count, citation_group)))
 
         index+=1
     
